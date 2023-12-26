95a6a40c0b48253dfdbdd16f7ec21cec729561a9
moyanyuansu <moyanyuansu@qq.com>
2023-12-26 10:28:28 +0800

    Update ipmi.sh
    1. 优化了输出内容
    2. 新增了调整到风扇自动模式
    3. 新增了服务器电源状态查看
    4. 新增了服务器电源操作

--- ipmi.sh
+++ ipmi.sh
@@ -15,16 +15,25 @@ function get_status(){
 	ipmitool -I lanplus -H $IDRAC_IP -U $username -P $password sdr type temperature
 	ipmitool -I lanplus -H $IDRAC_IP -U $username -P $password sdr type Voltage | grep Voltage
 	ipmitool -I lanplus -H $IDRAC_IP -U $username -P $password sdr type current
+ 	ipmitool -I lanplus -H $IDRAC_IP -U $username -P $password power status
   pause
   menu
 }
 
-function set_fan_speed(){
+function set_man_fan_speed(){
   echo -n "Enter fans speed percent(0-100):"
   read percent
   hexpercent=`printf '%x\n' $percent`
-  ipmitool -I lanplus -H $IDRAC_IP -U $username -P $password raw 0x30 0x30 0x01 0x00
-  ipmitool -I lanplus -H $IDRAC_IP -U $username -P $password raw 0x30 0x30 0x02 0xff 0x$hexpercent
+  nohup ipmitool -I lanplus -H $IDRAC_IP -U $username -P $password raw 0x30 0x30 0x01 0x00  > /dev/null 2>&1 &
+  nohup ipmitool -I lanplus -H $IDRAC_IP -U $username -P $password raw 0x30 0x30 0x02 0xff 0x$hexpercent  > /dev/null 2>&1 &
+  echo "Fan speed set to manually mode, speed is $percent%"
+  pause
+  menu
+}
+
+function set_auto_fan_speed(){
+  nohup ipmitool -I lanplus -H $IDRAC_IP -U $username -P $password raw 0x30 0x30 0x01 0x01  > /dev/null 2>&1 &
+  echo "Fan speed set to automatic mode"
   pause
   menu
 }
@@ -41,14 +50,120 @@ function get_temperature(){
   menu
 }
 
-function get_power(){
+function get_power_consumption(){
   ipmitool -I lanplus -H $IDRAC_IP -U $username -P $password sdr type current | grep Watts
   pause
   menu
 }
 
-function menu ()
-{
+function get_power_status(){
+  ipmitool -I lanplus -H $IDRAC_IP -U $username -P $password power status
+  pause
+  menu
+}
+
+function set_power_off(){
+  nohup ipmitool -I lanplus -H $IDRAC_IP -U $username -P $password power off > /dev/null 2>&1 &
+  echo "The power of server is turnning off"
+  pause
+  menu
+}
+
+function set_power_off_soft(){
+  nohup ipmitool -I lanplus -H $IDRAC_IP -U $username -P $password power soft > /dev/null 2>&1 &
+  echo "The power of server is turnning off softly"
+  pause
+  menu
+}
+
+function set_power_on(){
+  nohup ipmitool -I lanplus -H $IDRAC_IP -U $username -P $password power on > /dev/null 2>&1 &
+  echo "The power of server is turnning on"
+  pause
+  menu
+}
+
+function set_power_reset(){
+  nohup ipmitool -I lanplus -H $IDRAC_IP -U $username -P $password power reset > /dev/null 2>&1 &
+  echo "The power of server is resetting"
+  pause
+  menu
+}
+
+function set_power_cycle(){
+  nohup ipmitool -I lanplus -H $IDRAC_IP -U $username -P $password power cycle > /dev/null 2>&1 &
+  echo "The power of server will be turnning off and then on after 1s"
+  pause
+  menu
+}
+
+function fan_submenu(){
+  cat << EOF
+----------------------------------------
+|***************SUBMENU****************|
+----------------------------------------
+`echo -e "\033[35m 1)Auto Fan Speed\033[0m"`
+`echo -e "\033[35m 2)Manually Fan Speed\033[0m"`
+`echo -e "\033[35m 3)Back Menu\033[0m"`
+`echo -e "\033[35m 4)EXIT\033[0m"`
+EOF
+read -p "Please enter the number:" num2
+case $num2 in
+    1)
+      set_auto_fan_speed
+      ;;
+    2)
+      set_man_fan_speed
+      ;;
+    3)
+      clear
+      menu
+      ;;
+    4)
+      exit 0
+esac
+}
+
+function power_submenu(){
+  cat << EOF
+----------------------------------------
+|***************SUBMENU****************|
+----------------------------------------
+`echo -e "\033[35m 1)Power Off\033[0m"`
+`echo -e "\033[35m 2)Power Off Soft\033[0m"`
+`echo -e "\033[35m 3)Power On\033[0m"`
+`echo -e "\033[35m 4)Power Reset\033[0m"`
+`echo -e "\033[35m 5)Power Cycle\033[0m"`
+`echo -e "\033[35m 6)Back Menu\033[0m"`
+`echo -e "\033[35m 7)EXIT\033[0m"`
+EOF
+read -p "Please enter the number:" num3
+case $num3 in
+    1)
+      set_power_off
+      ;;
+    2)
+      set_power_off_soft
+      ;;
+    3)
+      set_power_on
+      ;;
+    4)
+      set_power_reset
+      ;;
+    5)
+      set_power_cycle
+      ;;
+    6)
+      clear
+      menu
+      ;;
+    7)
+      exit 0
+esac
+}
+
+function menu (){
     cat << EOF
 ----------------------------------------
 |*****************MENU*****************|
@@ -57,9 +172,11 @@ function menu ()
 `echo -e "\033[35m 2)Set Fan Speed\033[0m"`
 `echo -e "\033[35m 3)Get Fan Speed\033[0m"`
 `echo -e "\033[35m 4)Get Server Temperature\033[0m"`
-`echo -e "\033[35m 5)Get Server Power\033[0m"`
-`echo -e "\033[35m 6)Back Menu\033[0m"`
-`echo -e "\033[35m 7)EXIT\033[0m"`
+`echo -e "\033[35m 5)Get Server Power Consumption\033[0m"`
+`echo -e "\033[35m 6)Set Server Power Status\033[0m"`
+`echo -e "\033[35m 7)Get Server Power Status\033[0m"`
+`echo -e "\033[35m 8)Back Menu\033[0m"`
+`echo -e "\033[35m 9)EXIT\033[0m"`
 EOF
 read -p "Please enter the number:" num1
 case $num1 in
@@ -67,7 +184,7 @@ case $num1 in
       get_status
       ;;
     2)
-      set_fan_speed
+      fan_submenu
       ;;
     3)
       get_fan_speed
@@ -76,13 +193,19 @@ case $num1 in
       get_temperature
       ;;
     5)
-      get_power
+      get_power_consumption
       ;;
     6)
+      power_submenu
+      ;;
+    7)
+      get_power_status
+      ;;
+    8)
       clear
       menu
       ;;
-    7)
+    9)
       exit 0
 esac
 }
